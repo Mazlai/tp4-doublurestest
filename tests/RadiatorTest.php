6278cce7b3dc6d2e24818d6023d835f2b8b98dbf
Mazlai <micky31200@gmail.com>
2023-03-23 13:09:37 +0100

    Termine

--- tests/RadiatorTest.php
+++ tests/RadiatorTest.php
@@ -5,16 +5,23 @@ namespace Tests;
 use PHPUnit\Framework\TestCase;
 use iut\Radiator;
 use iut\Thermometer;
+use iut\iThermometer;
 
 use \Mockery\Adapter\Phpunit\MockeryTestCase;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 class RadiatorTest extends MockeryTestCase
-{
+{   
+
     public function testUpdateThermostat()
     {
-        $radiator = new Radiator(new Thermometer());
+
+        $mockThermometer = \Mockery::mock('Thermometer', 'iThermometer');
+
+        $radiator = new Radiator($mockThermometer);
+
+        $mockThermometer->shouldReceive('outsideTemperature')->andReturn(5);
 
         $radiator->updateThermostatFromOutsideTemperature();
 
